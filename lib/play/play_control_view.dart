d3a9dca5f2d629faf589cf69609d5f17a40cf3c4
王方 <wangfang@sengled.com>
2022-07-12 18:11:21 +0800

    add play control widget

---
+++ lib/play/play_control_view.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class PlayControlView extends StatefulWidget {
+  const PlayControlView({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  _PlayControlViewState createState() {
+    return _PlayControlViewState();
+  }
+}
+
+class _PlayControlViewState extends State<PlayControlView>
+    with TickerProviderStateMixin {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+}
